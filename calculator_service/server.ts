2bb8eba2cffb7bd5e89009d6698197f22fbcfb2b
Mieczyslaw Suska <mieczyslaw.suska@capgemini.com>
2019-11-27 23:26:26 +0100

    init server for calculation

--- calculator_service/server.ts
+++ calculator_service/server.ts
@@ -1,1 +1,10 @@
-console.log("HEllo from service");
+import * as restify from "restify";
+
+const server = restify.createServer();
+server.get("/calculate", (req, res) => {
+    res.send("calculated");
+})
+
+server.listen(3001, () => {
+    console.log(`\n ${ server.name } listening to ${ server.url }`);
+});
